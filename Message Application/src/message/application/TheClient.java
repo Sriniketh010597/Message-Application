b7f3917e25fcc92d74fd92abcec3066c1eb819c4
SRINIKETH VENKATESH <120231325+Sriniketh010597@users.noreply.github.com>
2025-02-04 15:19:27 -0500

    Update TheClient.java
    Improved variable and other naming for better understanding and consistency.

--- Message Application/src/message/application/TheClient.java
+++ Message Application/src/message/application/TheClient.java
@@ -11,8 +11,8 @@ import java.text.*;
 public class TheClient extends JFrame implements ActionListener {
     
     JTextField messagefield;
-    static JPanel a1;
-    static Box vertical = Box.createVerticalBox();
+    static JPanel chatpanel;
+    static Box verticalscale = Box.createVerticalBox();
    
     
         TheClient() {
@@ -28,11 +28,11 @@ public class TheClient extends JFrame implements ActionListener {
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/3.png"));
         Image i2 = i1.getImage().getScaledInstance(25, 25, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
-        JLabel back = new JLabel(i3);
-        back.setBounds(5, 20, 25, 25);
-        headerpanel.add(back);
+        JLabel backbutton = new JLabel(i3);
+        backbutton.setBounds(5, 20, 25, 25);
+        headerpanel.add(backbutton);
         
-        back.addMouseListener(new MouseAdapter() {
+        backbutton.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent ae) {
                 System.exit(0);
             }
@@ -41,59 +41,59 @@ public class TheClient extends JFrame implements ActionListener {
         ImageIcon i4 = new ImageIcon(ClassLoader.getSystemResource("icons/circle.jpg"));
         Image i5 = i4.getImage().getScaledInstance(53, 47, Image.SCALE_DEFAULT);
         ImageIcon i6 = new ImageIcon(i5);
-        JLabel profile = new JLabel(i6);
-        profile.setBounds(40, 10, 50, 50);
-        headerpanel.add(profile);
+        JLabel profilepic = new JLabel(i6);
+        profilepic.setBounds(40, 10, 50, 50);
+        headerpanel.add(profilepic);
         
         ImageIcon i7 = new ImageIcon(ClassLoader.getSystemResource("icons/video.png"));
         Image i8 = i7.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
         ImageIcon i9 = new ImageIcon(i8);
-        JLabel video = new JLabel(i9);
-        video.setBounds(300, 20, 30, 30);
-        headerpanel.add(video);
+        JLabel videocall = new JLabel(i9);
+        videocall.setBounds(300, 20, 30, 30);
+        headerpanel.add(videocall);
         
         ImageIcon i10 = new ImageIcon(ClassLoader.getSystemResource("icons/phone.png"));
         Image i11 = i10.getImage().getScaledInstance(35, 30, Image.SCALE_DEFAULT);
         ImageIcon i12 = new ImageIcon(i11);
-        JLabel phone = new JLabel(i12);
-        phone.setBounds(360, 20, 35, 30);
-        headerpanel.add(phone);
+        JLabel telephone = new JLabel(i12);
+        telephone.setBounds(360, 20, 35, 30);
+        headerpanel.add(telephone);
         
         ImageIcon i13 = new ImageIcon(ClassLoader.getSystemResource("icons/3icon.png"));
         Image i14 = i13.getImage().getScaledInstance(10, 25, Image.SCALE_DEFAULT);
         ImageIcon i15 = new ImageIcon(i14);
-        JLabel morevert = new JLabel(i15);
-        morevert.setBounds(420, 20, 10, 25);
-        headerpanel.add(morevert);
+        JLabel mrevert = new JLabel(i15);
+        mrevert.setBounds(420, 20, 10, 25);
+        headerpanel.add(mrevert);
         
-        JLabel name = new JLabel("Sriniketh");
-        name.setBounds(110, 15, 100, 18);
-        name.setForeground(Color.WHITE);
-        name.setFont(new Font("SAN_SERIF", Font.BOLD, 18));
-        headerpanel.add(name);
+        JLabel username = new JLabel("Sriniketh");
+        username.setBounds(110, 15, 100, 18);
+        username.setForeground(Color.WHITE);
+        username.setFont(new Font("SAN_SERIF", Font.BOLD, 18));
+        headerpanel.add(username);
         
-        JLabel status = new JLabel("Active Now");
-        status.setBounds(110, 35, 100, 18);
-        status.setForeground(Color.WHITE);
-        status.setFont(new Font("SAN_SERIF", Font.BOLD, 14));
-        headerpanel.add(status);
+        JLabel statusindicator = new JLabel("Online");
+        statusindicator.setBounds(110, 35, 100, 18);
+        statusindicator.setForeground(Color.WHITE);
+        statusindicator.setFont(new Font("SAN_SERIF", Font.BOLD, 14));
+        headerpanel.add(statusindicator);
         
-        a1 = new JPanel();
-        a1.setBounds(5, 75, 440, 570);
-        add(a1);
+        chatpanel = new JPanel();
+        chatpanel.setBounds(5, 75, 440, 570);
+        add(chatpanel);
         
         messagefield = new JTextField();
         messagefield.setBounds(5, 655, 310, 40);
         messagefield.setFont(new Font("SAN_SERIF", Font.PLAIN, 16));
         add(messagefield);
         
-        JButton send = new JButton("Send");
-        send.setBounds(320, 655, 123, 40);
-        send.setBackground(new Color(7, 94, 84));
-        send.setForeground(Color.WHITE);
-        send.addActionListener(this);
-        send.setFont(new Font("SAN_SERIF", Font.PLAIN, 16));
-        add(send);
+        JButton sendmessage = new JButton("Send");
+        sendmessage.setBounds(320, 655, 123, 40);
+        sendmessage.setBackground(new Color(7, 94, 84));
+        sendmessage.setForeground(Color.WHITE);
+        sendmessage.addActionListener(this);
+        sendmessage.setFont(new Font("SAN_SERIF", Font.PLAIN, 16));
+        add(sendmessage);
         
         setSize(450, 700);
         setLocation(800, 50);
@@ -107,16 +107,16 @@ public class TheClient extends JFrame implements ActionListener {
         try {
             String out = messagefield.getText();
 
-            JPanel p2 = formatLabel(out);
+            JPanel panel2 = formatLabel(out);
 
-            a1.setLayout(new BorderLayout());
+            chatpanel.setLayout(new BorderLayout());
 
-            JPanel right = new JPanel(new BorderLayout());
-            right.add(p2, BorderLayout.LINE_END);
-            vertical.add(right);
-            vertical.add(Box.createVerticalStrut(15));
+            JPanel onright = new JPanel(new BorderLayout());
+            onright.add(panel2, BorderLayout.LINE_END);
+            verticalscale.add(onright);
+            verticalscale.add(Box.createVerticalStrut(15));
 
-            a1.add(vertical, BorderLayout.PAGE_START);
+            chatpanel.add(vertical, BorderLayout.PAGE_START);
 
             messagefield.setText("");
 
@@ -140,13 +140,13 @@ public class TheClient extends JFrame implements ActionListener {
         
         panel.add(output);
         
-        Calendar cal = Calendar.getInstance();
-        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        Calendar thecalender = Calendar.getInstance();
+        SimpleDateFormat date = new SimpleDateFormat("HH:mm");
         
-        JLabel time = new JLabel();
-        time.setText(sdf.format(cal.getTime()));
+        JLabel timeclock = new JLabel();
+        timeclock.setText(date.format(thecalender.getTime()));
         
-        panel.add(time);
+        panel.add(timeclock);
         
         return panel;
     }
